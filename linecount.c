7bad2f8da9bbbb53693d84de195f30f717f13a8f
Ryan Gamel <ryan.gamel@gmail.com>
2025-07-21 16:46:27 -0500

    1.8 complete

--- linecount.c
+++ linecount.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 int main() {
-	int c, nl;
+	int c, nl, t, s;
+
+	nl = 0;
+	t = 0;
+	s = 0;
 
 	nl = 0;
 	while ((c = getchar()) != EOF) {
-		printf("hello %d\n", c);
 		if (c == '\n') {
-			printf("found newline\n");
 			++nl;
+		} 
+		if (c == '\t') {
+			++t;
+		}
+		if (c == ' ') {
+			++s;
 		}
 	}
 
-	printf("%d\n", nl);
+	printf("newlines %d\n", nl);
+	printf("tabs %d\n", t);
+	printf("spaces %d\n", s);
 }
+
