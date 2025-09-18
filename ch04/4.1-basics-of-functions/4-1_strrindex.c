c442b7a024d8779715f23ca8651af1255066fa3c
Ryan Gamel <ryan.gamel.ext@bayer.com>
2025-09-18 12:51:57 -0500

    4-1 done

---
+++ ch04/4.1-basics-of-functions/4-1_strrindex.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int strrindex(char s[], char t) {
+  int i;
+  int count = -1;
+
+  for (i = 0; s[i] != '\0'; ++i) {
+    if (s[i] == t) {
+      count = i;
+    }
+  }
+
+  return count;
+}
+
+int main(void) {
+  char test[] = "Hello, world!";
+  printf("\n%s, %c, %d\n", test, 'o', strrindex(test, 'o'));
+  return 0;
+}
